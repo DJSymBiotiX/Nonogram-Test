f164d69d6e84fc1c36e14a2b2e568150fc525107
Fernando Chorney <gitlab@djsbx.com>
2018-02-16 22:38:52 -0600

    Got the clue numbers up and make the board smaller
    - Need to clean up the code, and find a way to scale the game board
      better.

--- Assets/SceneState.cs
+++ Assets/SceneState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Linq;
 using System.IO; // The System.IO namespace contains functions related to loading and saving files
 
@@ -11,73 +12,116 @@ public class SceneState : MonoBehaviour {
 
 	public string level_file = "";
 	public GameObject square_prefab;
+	public Canvas canvas;
 
 	int[,] grid_data;
 	GameObject[,] grid_squares;
-	List<int>[] rows;
-	List<int>[] columns; 
-	int x = 0;
-	int y = 0;
+	List<int>[] row_clues;
+	List<int>[] column_clues; 
+	int length = 0;
+	int height = 0;
 
 	// Use this for initialization
 	void Start () {
-		LoadGameData ();
-		LoadTiles ();		
+		LoadPuzzleData ();
+		FindPuzzleClues ();
+		LoadTiles ();
+		LoadClues ();
 	}
 
 	void output_rows() {
 		string output = "All Rows\r\n";
-		for (int i = 0; i < y ; i++) {
-			output += "Row [" + i + "]: " + String.Join (" ", rows[i].Select(x => x.ToString()).ToArray()) + "\r\n";
+		for (int i = 0; i < height ; i++) {
+			output += "Row [" + i + "]: " + String.Join (" ", row_clues[i].Select(x => x.ToString()).ToArray()) + "\r\n";
 		}
 		Debug.Log (output);
 	}
 
 	void output_columns() {
 		string output = "All Cols\r\n";
-		for (int i = 0; i < x ; i++) {
-			output += "Col [" + i + "]: " + String.Join (" ", columns [i].Select (x => x.ToString ()).ToArray ()) + "\r\n";
+		for (int i = 0; i < length ; i++) {
+			output += "Col [" + i + "]: " + String.Join (" ", column_clues [i].Select (x => x.ToString ()).ToArray ()) + "\r\n";
 		}
 		Debug.Log (output);
 	}
 
 	void LoadTiles() {
 		int offset = 1;
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
-				Vector3 pos = new Vector3 ((offset * i) - (x / 2), (offset * j) - (y / 2), 1);
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < height; j++) {
+				Vector3 pos = new Vector3 ((offset * i) - (i * 0.5f) - (length / 4), (offset * j)  - (j * 0.5f) - (height / 4), 1);
 				grid_squares [i, j] = Instantiate (square_prefab, pos, Quaternion.identity);
 			}
 		}
+	}
 
+	void LoadClues(){
+		int fontsize = 20;
+
+		string str = "";
+
+		for (int i = 0; i < row_clues.Length; i++) {
+			for (int j = row_clues[i].Count - 1 ; j >= 0 ; j--) {
+				GameObject ngo = new GameObject ("myTextGO");
+				ngo.transform.SetParent (canvas.transform);
+				Text myText = ngo.AddComponent<Text> ();
+				myText.color = Color.black;
+				myText.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+				myText.fontSize = fontsize;
+				myText.text = row_clues [i] [j].ToString ();
+				RectTransform rt = myText.GetComponent<RectTransform> ();
+				rt.position = new Vector3 (-2.3f - ((row_clues[i].Count - j) * 0.2f), 1.5f - i + (i * 0.5f), 1);
+				rt.localScale = new Vector3 (1, 1, 1);
+				rt.sizeDelta = new Vector2 (myText.preferredWidth, myText.preferredHeight);
+			}
+		}
+
+		for (int i = 0; i < column_clues.Length; i++) {
+			for (int j = column_clues [i].Count - 1; j >= 0; j--) {
+				GameObject ngo = new GameObject ("myTextGO");
+				ngo.transform.SetParent (canvas.transform);
+				Text myText = ngo.AddComponent<Text> ();
+				myText.color = Color.black;
+				myText.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+				myText.fontSize = fontsize;
+				myText.text = column_clues [i] [j].ToString ();;
+				RectTransform rt = myText.GetComponent<RectTransform> ();
+				rt.position = new Vector3 (-2 + i - (i * 0.5f), 1.9f + ((column_clues[i].Count - j) * 0.2f), 1);
+				rt.localScale = new Vector3 (1, 1, 1);
+				rt.sizeDelta = new Vector2 (myText.preferredWidth, myText.preferredHeight);
+			}
+		}
+	}
+		
+	void FindPuzzleClues() {
 		// Performance: This is garbage, we need to make it better and/or faster somehow
 		int row_counter = 0;
-		for (int i = 0; i < x; i++) {
-			for (int j = 0; j < y; j++) {
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < height; j++) {
 				if (grid_data [i, j] == 1) {
 					row_counter++;
 				} 
-				if (grid_data [i, j] == 0 || j == y - 1) {
+				if (grid_data [i, j] == 0 || j == height - 1) {
 					if (row_counter != 0)
-						rows[i].Add(row_counter);
+						row_clues[i].Add(row_counter);
 					row_counter = 0;
 				}
 			}
 		}
 		int column_counter = 0;
-		for (int i = 0; i < y; i++) {
-			for (int j = 0; j < x; j++) {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < length; j++) {
 				if (grid_data [j, i] == 1) {
 					column_counter++;
 				}
-				if (grid_data[j, i] == 0 || j == x - 1) {
+				if (grid_data[j, i] == 0 || j == length - 1) {
 					if (column_counter != 0 )
-						columns[i].Add(column_counter);
+						column_clues[i].Add(column_counter);
 					column_counter = 0;
 				}
 			}
 		}
-			
+
 		output_rows ();
 		output_columns ();
 	}
@@ -102,44 +146,40 @@ public class SceneState : MonoBehaviour {
 		Debug.Log(output);
 	}
 
-	private void LoadGameData()
-	{
+	private void LoadPuzzleData()	{
 		// Path.Combine combines strings into a file path
 		// Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
 		string filePath = Path.Combine(Application.streamingAssetsPath, "levels");
 		filePath = Path.Combine(filePath, level_file + ".json");
 
-		if(File.Exists(filePath))
-		{
+		if (File.Exists(filePath)) {
 			// Read the json from the file into a string
 			string dataAsJson = File.ReadAllText(filePath); 
 			// Pass the json to JsonUtility, and tell it to create a GameData object from it
 			LevelData loadedData = JsonUtility.FromJson<LevelData>(dataAsJson);
 			setGridData (loadedData);
-		}
-		else
-		{
+		} else {
 			Debug.LogError("Cannot load game data!");
 		}
 	}
 
 	void setGridData(LevelData loadedData) {
-		x = loadedData.length;
-		y = loadedData.height;
+		length = loadedData.length;
+		height = loadedData.height;
 
 		// Initialize our arrays
-		grid_data = new int[x, y];
-		grid_squares = new GameObject[x, y];
-		columns = new List<int>[x];
-		rows = new List<int>[y];
+		grid_data = new int[length, height];
+		grid_squares = new GameObject[length, height];
+		column_clues = new List<int>[length];
+		row_clues = new List<int>[height];
 	
 		// Put our data into our grid_data rray
 		int count = 0;
-		for (int i = 0; i < x; i++) {
-			columns [i] = new List<int> ();
-			for (int j = 0; j < y; j++) {
+		for (int i = 0; i < length; i++) {
+			column_clues [i] = new List<int> ();
+			for (int j = 0; j < height; j++) {
 				grid_data [i, j] = loadedData.data [count++];
-				rows [j] = new List<int> ();
+				row_clues [j] = new List<int> ();
 			}
 		}
 	}
